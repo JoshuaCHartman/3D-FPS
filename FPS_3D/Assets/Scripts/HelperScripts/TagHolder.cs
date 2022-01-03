6e4d8f749012fe692a7f3015033ccc7165f78d3e
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-03 17:55:00 -0500

    Basic Mvmt
    wasd movement
    helper script

---
+++ FPS_3D/Assets/Scripts/HelperScripts/TagHolder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis
+{
+    // tags for axis - reduces mistakes, reduces hardcoding
+
+    public const string HORIZONTAL = "Horizontal";
+    public const string VERTICAL = "Vertical";
+}
--- FPS_3D/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ FPS_3D/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private CharacterController characterController;
+    private Vector3 moveDirection;
 
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jumpForce = 10f;
+    private float verticalVelocity;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>(); // reference to Player Character Controller component
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+        // GetAxis - incremental values vs GetAxisRaw - whole integer (-1,0,1)
+        moveDirection = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL)); // (x,y,z) using TagHolder helper script
+
+        moveDirection = transform.TransformDirection(moveDirection); // change from local to global directions
+        moveDirection *= speed * Time.deltaTime;
+
+        characterController.Move(moveDirection);
+
+
     }
 }
