4fb7564a9e225cba41e67f1ad43fb6b6f3460e0d
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-06 22:54:06 -0500

    NavMesh & Start scripts
    - Baked walkable navmesh
    - created enemy animations script w/ idle, run, walk, attack
    - created animator state machines w/ transitions
    - created enemy controller script

---
+++ FPS_3D/Assets/Scripts/EnemyScripts/EnemyAnimator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void Walk(bool walk)
+    {
+        anim.SetBool(AnimationTags.WALK_PARAMETER, walk);
+    }
+    public void Run(bool run)
+    {
+        anim.SetBool(AnimationTags.RUN_PARAMETER, run);
+    }
+    public void Attack() // trigger
+    {
+        anim.SetTrigger(AnimationTags.ATTACK_TRIGGER);
+    }
+    public void Dead()
+    {
+        anim.SetTrigger(AnimationTags.DEAD_TRIGGER);
+    }
+
+}
