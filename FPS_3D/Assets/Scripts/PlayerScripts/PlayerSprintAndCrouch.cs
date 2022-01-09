36b53873c793e7b1c8bc52416eed8e93b8934736
JoshuaCHartman <73659857+JoshuaCHartman@users.noreply.github.com>
2022-01-08 20:22:22 -0500

    Stamina
    - added stamina value to sprint script
    - methods to check if shift pressed and if so, reduce stamina at a set rate
    - when shift up, stamina regens
    
    display on stamina bar

--- FPS_3D/Assets/Scripts/PlayerScripts/PlayerSprintAndCrouch.cs
+++ FPS_3D/Assets/Scripts/PlayerScripts/PlayerSprintAndCrouch.cs
@@ -25,7 +25,12 @@ public class PlayerSprintAndCrouch : MonoBehaviour
     private float _sprintStepDistance = 0.25f;
     private float _crouchStepDistance = 0.5f;
 
-    void Awake()
+    // Stamina & UI Stamina
+    private PlayerStatistics _playerStatistics;
+    private float _sprintValue = 100f;
+    public float sprintThreshold = 10f;
+
+    void Awake() // get references
     {
         _playerMovement = GetComponent<PlayerMovement>();
 
@@ -33,14 +38,12 @@ public class PlayerSprintAndCrouch : MonoBehaviour
 
         _playerFootsteps = GetComponentInChildren<PlayerFootsteps>(); // playerfootsteps script in playeraudio object
 
+        _playerStatistics = GetComponent<PlayerStatistics>();
     }
 
     void Start()
     {
         WalkStepDefault();
-        //_playerFootsteps.stepDistance = _walkStepDistance;
-        //_playerFootsteps.volumeMin = _walkVolumeMin;
-        //_playerFootsteps.volumeMax = _walkVolumeMax;
     }
 
     void WalkStepDefault()
@@ -50,7 +53,6 @@ public class PlayerSprintAndCrouch : MonoBehaviour
         _playerFootsteps.volumeMax = _walkVolumeMax;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Sprint();
@@ -69,12 +71,7 @@ public class PlayerSprintAndCrouch : MonoBehaviour
                 _playerMovement.speed = moveSpeed;
                 WalkStepDefault();
 
-                //_playerFootsteps.stepDistance = _walkStepDistance;
-                //_playerFootsteps.volumeMin = _walkVolumeMin;
-                //_playerFootsteps.volumeMax = _walkVolumeMax;
-
                 _isCrouching = false;
-
 
             }
             else
@@ -95,199 +92,60 @@ public class PlayerSprintAndCrouch : MonoBehaviour
 
     void Sprint()
     {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !_isCrouching)
+        // check if have stamina, can sprint
+        if (_sprintValue > 0f)
         {
-            _playerMovement.speed = sprintSpeed;
-            _playerFootsteps.stepDistance = _sprintStepDistance;
-            _playerFootsteps.volumeMin = _sprintVolume; // sound louder when sprinting
-            _playerFootsteps.volumeMax = _sprintVolume;
+            if (Input.GetKeyDown(KeyCode.LeftShift) && !_isCrouching)
+            {
+                _playerMovement.speed = sprintSpeed;
+                _playerFootsteps.stepDistance = _sprintStepDistance;
+                _playerFootsteps.volumeMin = _sprintVolume; // sound louder when sprinting
+                _playerFootsteps.volumeMax = _sprintVolume;
+            }
+
         }
 
         if (Input.GetKeyUp(KeyCode.LeftShift) && !_isCrouching)
         {
             _playerMovement.speed = moveSpeed;
-            //WalkStepDefault();
-            //_playerFootsteps.stepDistance = _walkStepDistance;
-            //_playerFootsteps.volumeMin = _walkVolumeMin;
-            //_playerFootsteps.volumeMax = _walkVolumeMax;
+            WalkStepDefault();
+        }
 
+        if (Input.GetKey(KeyCode.LeftShift) && !_isCrouching) // checks anytime left shift is pressed
+        {
+            // lose stamina at rate determined by sprintThreshold
+            _sprintValue -= Time.deltaTime * sprintThreshold;
+
+            // Check level & ensure _sprintvalue does not go below 0
+            if (_sprintValue <= 0)
+            {
+                _sprintValue = 0; 
+
+                // reset sprint speed & sound
+                _playerMovement.speed = moveSpeed;
+                WalkStepDefault();
+            }
+            // display decrease stamina on bar
+            _playerStatistics.DisplayStaminaStats(_sprintValue);
+        }
+        else
+        {
+            if (_sprintValue != 100f) // if not full stamina
+            {
+                // regnerate stamina but at lower rate than lost
+                _sprintValue += (sprintThreshold / 2f) * Time.deltaTime;
+
+                // check stamina level & ensure not more than 100
+                if (_sprintValue > 100f)
+                {
+                    _sprintValue = 100f;
+                }
+
+                // display regen stamina on bar
+                _playerStatistics.DisplayStaminaStats(_sprintValue);
+            }
         }
     }
 
 
 }
-
-
-
-
-//{
-
-//    private PlayerMovement _playerMovement;
-
-//    public float sprint_Speed = 10f;
-//    public float move_Speed = 5f;
-//    public float crouch_Speed = 2f;
-
-//    private Transform look_Root;
-//    private float stand_Height = 1.6f;
-//    private float crouch_Height = 1f;
-
-//    private bool is_Crouching;
-
-//    private PlayerFootsteps player_Footsteps;
-
-//    private float sprint_Volume = 1f;
-//    private float crouch_Volume = 0.1f;
-//    private float walk_Volume_Min = 0.2f, walk_Volume_Max = 0.6f;
-
-//    private float walk_Step_Distance = 0.4f;
-//    private float sprint_Step_Distance = 0.25f;
-//    private float crouch_Step_Distance = 0.5f;
-
-//    // private PlayerStats player_Stats;
-
-//    private float sprint_Value = 100f;
-//    public float sprint_Treshold = 10f;
-
-//    void Awake()
-//    {
-
-//        _playerMovement = GetComponent<PlayerMovement>();
-
-//        look_Root = transform.GetChild(0);
-
-//        player_Footsteps = GetComponentInChildren<PlayerFootsteps>();
-
-//        // player_Stats = GetComponent<PlayerStats>();
-
-//    }
-//    void Start()
-//    {
-//        player_Footsteps.volume_Min = walk_Volume_Min;
-//        player_Footsteps.volume_Max = walk_Volume_Max;
-//        player_Footsteps.step_Distance = walk_Step_Distance;
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-//        Sprint();
-//        Crouch();
-//    }
-
-//    void Sprint()
-//    {
-
-//        // if we have stamina we can sprint
-//        if (sprint_Value > 0f)
-//        {
-
-//            if (Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching)
-//            {
-
-//                _playerMovement.speed = sprint_Speed;
-
-//                player_Footsteps.step_Distance = sprint_Step_Distance;
-//                player_Footsteps.volume_Min = sprint_Volume;
-//                player_Footsteps.volume_Max = sprint_Volume;
-
-//            }
-
-//        }
-
-//        if (Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching)
-//        {
-
-//            _playerMovement.speed = move_Speed;
-
-//            player_Footsteps.step_Distance = walk_Step_Distance;
-//            player_Footsteps.volume_Min = walk_Volume_Min;
-//            player_Footsteps.volume_Max = walk_Volume_Max;
-
-//        }
-
-//        if (Input.GetKey(KeyCode.LeftShift) && !is_Crouching)
-//        {
-
-//            sprint_Value -= sprint_Treshold * Time.deltaTime;
-
-//            if (sprint_Value <= 0f)
-//            {
-
-//                sprint_Value = 0f;
-
-//                // reset the speed and sound
-//                _playerMovement.speed = move_Speed;
-//                player_Footsteps.step_Distance = walk_Step_Distance;
-//                player_Footsteps.volume_Min = walk_Volume_Min;
-//                player_Footsteps.volume_Max = walk_Volume_Max;
-
-
-//            }
-
-//            // player_Stats.Display_StaminaStats(sprint_Value);
-
-//        }
-//        else
-//        {
-
-//            if (sprint_Value != 100f)
-//            {
-
-//                sprint_Value += (sprint_Treshold / 2f) * Time.deltaTime;
-
-//                //player_Stats.Display_StaminaStats(sprint_Value);
-
-//                if (sprint_Value > 100f)
-//                {
-//                    sprint_Value = 100f;
-//                }
-
-//            }
-
-//        }
-
-
-//    } // sprint
-
-//    void Crouch()
-//    {
-
-//        if (Input.GetKeyDown(KeyCode.C))
-//        {
-
-//            // if we are crouching - stand up
-//            if (is_Crouching)
-//            {
-
-//                look_Root.localPosition = new Vector3(0f, stand_Height, 0f);
-//                _playerMovement.speed = move_Speed;
-
-//                player_Footsteps.step_Distance = walk_Step_Distance;
-//                player_Footsteps.volume_Min = walk_Volume_Min;
-//                player_Footsteps.volume_Max = walk_Volume_Max;
-
-//                is_Crouching = false;
-
-//            }
-//            else
-//            {
-//                // if we are not crouching - crouch
-
-//                look_Root.localPosition = new Vector3(0f, crouch_Height, 0f);
-//                _playerMovement.speed = crouch_Speed;
-
-//                player_Footsteps.step_Distance = crouch_Step_Distance;
-//                player_Footsteps.volume_Min = crouch_Volume;
-//                player_Footsteps.volume_Max = crouch_Volume;
-
-//                is_Crouching = true;
-
-//            }
-
-//        } // if we press c
-
-
-//    } // crouch
-
-//} // class
